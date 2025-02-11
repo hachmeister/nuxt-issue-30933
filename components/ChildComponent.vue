07011baa8ca6d8d721b5f738c533fdb7538b6ba9
Karsten Hachmeister <karsten.hachmeister@tib.eu>
2025-02-11 10:06:30 +0100

    initial commit

---
+++ components/ChildComponent.vue
@@ -0,0 +1,9 @@
+<script setup>
+await useFetch('https://google.com')
+const emit = defineEmits(['updateContent'])
+emit('updateContent', "after")
+</script>
+
+<template>
+  <h2>ChildComponent</h2>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<script setup>
+const content = ref('before')
+//const content = useState(() => 'before')
+</script>
+
+<template>
+  <div>
+    <ChildComponent @update-content="content = $event"/>
+    {{ content }}
+  </div>
+</template>
